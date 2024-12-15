f6798ccf5668cd7e7bfc1c143c5df61b59bb33bb
veresoora <lahanabira@gmail.com>
2024-12-15 17:32:18 +0700

    Untuk mengelola data mahasiswa dari MahasiswaDao

--- app/src/main/java/com/nadia/pertemuan8/repository/RepositoryMhs.kt
+++ app/src/main/java/com/nadia/pertemuan8/repository/RepositoryMhs.kt
@@ -1,7 +1,16 @@
 package com.nadia.pertemuan8.repository
 
 import com.nadia.pertemuan8.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    fun getAllMhs () : Flow<List<Mahasiswa>>
+
+    fun getMhs(nim: String) : Flow<Mahasiswa>
+
+    suspend fun deletMhs (mahasiswa: Mahasiswa)
+
+    suspend fun updateMhs (mahasiswa: Mahasiswa)
 }
