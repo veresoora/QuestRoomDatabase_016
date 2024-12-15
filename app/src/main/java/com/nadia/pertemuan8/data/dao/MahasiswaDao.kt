66ca26f18a953d5c724283eff4d47e09e75c4dbc
veresoora <lahanabira@gmail.com>
2024-12-15 17:28:53 +0700

    Untuk memperbarui informasi mahasiswa yang sudah ada didatabase

--- app/src/main/java/com/nadia/pertemuan8/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/nadia/pertemuan8/data/dao/MahasiswaDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.nadia.pertemuan8.data.entity.Mahasiswa
 import kotlinx.coroutines.flow.Flow
 
@@ -20,4 +21,7 @@ interface MahasiswaDao {
 
     @Delete
     suspend fun deleteMahasiswa (mahasiswa: Mahasiswa)
+
+    @Update
+    suspend fun updateMahasiswa (mahasiswa: Mahasiswa)
 }
