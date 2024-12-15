53e2d8ff7a8ea25b51a98ee215bc2bc0068f3567
veresoora <lahanabira@gmail.com>
2024-12-15 17:20:18 +0700

    Untuk mengambil semua data mahasiswa yang tersimpan didatabase

--- app/src/main/java/com/nadia/pertemuan8/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/nadia/pertemuan8/data/dao/MahasiswaDao.kt
@@ -2,10 +2,15 @@ package com.nadia.pertemuan8.data.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.nadia.pertemuan8.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MahasiswaDao {
     @Insert
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMahasiswa() : Flow<List<Mahasiswa>>
 }
