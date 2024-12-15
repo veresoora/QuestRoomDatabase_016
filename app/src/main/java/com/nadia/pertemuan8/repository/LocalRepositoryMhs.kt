1aeecee7e8efadff424cd6f234988e7ce43557f3
veresoora <lahanabira@gmail.com>
2024-12-15 17:53:00 +0700

    implementasi interface RepositoryMhs untuk mengelola data mahasiswa dari MahasiswaDao

--- app/src/main/java/com/nadia/pertemuan8/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/nadia/pertemuan8/repository/LocalRepositoryMhs.kt
@@ -2,11 +2,27 @@ package com.nadia.pertemuan8.repository
 
 import com.nadia.pertemuan8.data.dao.MahasiswaDao
 import com.nadia.pertemuan8.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs(
     private val mahasiswaDao: MahasiswaDao
 ) :RepositoryMhs {
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
+    }
+
+    override fun getAllMhs(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMahasiswa()
+    }
+    override fun getMhs(nim: String): Flow<Mahasiswa> {
+        return mahasiswaDao.getMahasiswa(nim)
+    }
+
+    override suspend fun deletMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.deleteMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.updateMahasiswa(mahasiswa)
     }
 }
